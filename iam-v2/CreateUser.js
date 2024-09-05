d34bb1e6ba9aac4514fc1a72971430aaead48374
Juan David <juandavid918@gmail.com>
2024-09-05 16:17:28 -0500

    Crear usuario IAM

---
+++ iam-v2/CreateUser.js
@@ -0,0 +1,16 @@
+const AWS = require("aws-sdk");
+
+const iam = new AWS.IAM();
+
+const params = {
+    UserName: "john"
+}
+
+iam.createUser(params, (err, data) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log(data);
+    }
+});
